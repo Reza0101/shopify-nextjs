b7501138f27560ee753c6e040bd133932cfb0cdf
RezaSiahi <rezasiahi8@gmail.com>
2023-08-18 20:17:04 +0330

    show product fake testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Product from "@/components/Product";
+import productData from "../data/products.json";
 
 const Home = () => {
   return (
     <>
       <Layout title="Home Page">
-        Home Page
+        <div className="flex gap-5">
+          {productData.map((product) => (
+            <Product key={product.id} item={product} />
+          ))}
+        </div>
       </Layout>
     </>
   );
