d97eff90b6e402c0ca125301c793cbcac24abf56
RezaSiahi <rezasiahi8@gmail.com>
2023-08-20 13:56:08 +0330

    show cart items in table

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -5,17 +5,40 @@ import { CartContext } from "@/context/cart";
 const Cart = () => {
   const { state, dispatch } = useContext(CartContext);
 
-  const { cart: {cartItems} } = state;
+  const {
+    cart: { cartItems },
+  } = state;
 
   console.log(cartItems);
 
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
-      {!!cartItems.length ? (
+      {!cartItems.length ? (
         <div>Cart is not empty.</div>
       ) : (
-        <div>Cart is empty.</div>
+        <>
+          <div className="container my-5 overflow-scroll">
+            <div className="flex justify-evenly p-1 w-[700px] mx-auto">
+              <p className="w-[50px] text-center">Item</p>
+              <p className="w-[50px] text-center">Quantity</p>
+              <p className="w-[50px] text-center">Image</p>
+              <p className="w-[50px] text-center">Price</p>
+              <p className="w-[50px] text-center">Action</p>
+            </div>
+            <div className="">
+              {cartItems.map((item: any) => (
+                <div className="flex p-1 w-[700px] mx-auto items-center justify-evenly">
+                  <p className="w-[50px] text-center">{item.title}</p>
+                  <p className="w-[50px] text-center">{item.qty}</p>
+                  <img className="rounded-lg w-[50px] text-center" src={item.image} />
+                  <p className="w-[50px] text-center">{item.price}$</p>
+                  <button className="w-[50px] text-center">Remove</button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
       )}
     </Layout>
   );
