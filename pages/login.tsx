343d2a7f821a0b4ae0b017a6e36864e5fbfe37b2
RezaSiahi <rezasiahi8@gmail.com>
2023-08-21 18:08:06 +0330

    add login page form validation

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,30 +1,71 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const loginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({
+    email,
+    password,
+  }: {
+    email?: string;
+    password?: number;
+  }) => {};
+
   return (
     <>
       <Layout title="Login">
         <div className="bg-white rounded-md p-4 w-[400px] md:w-[550px] mx-auto mt-10">
           <p className="text-center font-bold py-5 text-2xl">Log in</p>
           <hr />
-          <form action="">
-            <label htmlFor="" className="block mt-5">
-              Email
-            </label>
-            <input
-              className="w-full mt-1 outline-blue-700 py-1 pl-2 border rounded-md mb-5"
-              type="email"
-              placeholder="example@gmail.com"
-            />
-            <label htmlFor="" className="block">
-              Password
-            </label>
-            <input
-              className="w-full mt-1 outline-blue-700 py-1 pl-2 border rounded-md mb-5"
-              type="password"
-              placeholder="at lest 8 characters"
-            />
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <div className="mb-5">
+              <label htmlFor="" className="block mt-5">
+                Email
+              </label>
+              <input
+                {...register("email", {
+                  required: true,
+                  validate: {
+                    matchMedia: (v) =>
+                      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v),
+                  },
+                })}
+                className="w-full mt-1 outline-blue-700 py-1 pl-2 border rounded-md"
+                type="text"
+                placeholder="example@gmail.com"
+              />
+              {errors.email && (
+                <div className="text-red-600">
+                  <p>This is not Valid email address</p>
+                </div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="" className="block">
+                Password
+              </label>
+              <input
+              {...register('password', {
+                required: true,
+                minLength: 8,
+                maxLength: 12
+              })}
+                className="w-full mt-1 outline-blue-700 py-1 pl-2 border rounded-md"
+                type="password"
+                placeholder="at lest 8 characters"
+              />
+              {errors.password && (
+                <div className="text-red-600">
+                    <p>Password Invalid</p>
+                </div>
+              )}
+            </div>
             <button className="text-white bg-blue-700 w-full my-5 rounded-md py-1">
               Log in
             </button>
